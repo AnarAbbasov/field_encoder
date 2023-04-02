ebdfe71539034e07f61c970913e8c292fa588105
Anar <97862627+AnarAbbasov@users.noreply.github.com>
2023-04-02 07:28:43 -0700

    removed extra comments

--- field_encoder.ino
+++ field_encoder.ino
@@ -1,59 +1,38 @@
 #include <morse.h>
-
-//#include <PS2Keyboard.h>
-
 #include <PS2Keyboard.h>
-
-
-
-
-// include the library code:
-
 #include <LiquidCrystal.h>
-//#include < PS2Keyboard.h>
-
-
-
 
 // initialize the library by associating any needed LCD interface pin
-
 // with the arduino pin number it is connected to
-
-
 const int rs = 12, en = 11, d4 = 6, d5 = 5, d6 = 4, d7 = 2;
-
-
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-
 const int DataPin = 8;
 const int IRQpin =  3;
-
+//Initialise keyboard instance
 PS2Keyboard keyboard;
-
+//Define pins for morse sender instance
 #define PIN_STATUS  13
 #define PIN_SPEAKER 10
 
-
+//initialise morse sender 
 MorseSender *callsignSender;
 
-
-
 void setup() {
-
-
   // set up the LCD's number of columns and rows:
-
-
   lcd.begin(16, 2);
    delay(1000);
   keyboard.begin(DataPin, IRQpin);
   Serial.begin(9600);
   Serial.println("Keyboard Test:");
- lcd.print("Keyboard Test:");
-
- callsignSender = new SpeakerMorseSender(PIN_SPEAKER,600);
- callsignSender->setup();
-  callsignSender->setMessage(String("73 de kb3jcy "));
+  lcd.print("Morse Sender V1.0.0");
+  delay(1000);
+  lcd.print("Anar(c) 2023.04.02");
+  delay(2000);
+  lcd.clear();
+  lcd.print(">");
+  callsignSender = new SpeakerMorseSender(PIN_SPEAKER,600);
+  callsignSender->setup();
+  callsignSender->setMessage(String("KG7RNM "));
 
 }
 
@@ -101,9 +80,11 @@ if (keyboard.available()) {
       // otherwise, just print all normal characters
    Serial.print(c);
        lcd.print(c);
-       //lcd.print("Keyboard Test:");
-    delay(500);
-//lcd.clear();
+        callsignSender->setMessage(String(c));
+        callsignSender->startSending();
+       
+    
+
     }
   
 
